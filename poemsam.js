f8e05e921c2c1b4febed4c20942ab6d879874574
Sandra Quiles <squiles@bamboorose.com>
2016-09-22 19:32:24 -0400

    separate make html entitities into functions

--- poemsam.js
+++ poemsam.js
@@ -1,49 +1,44 @@
-'use strict';
+(function() {
+	var	$el = $("#newPoem"),
+			$print = $('#poemsam'),
+			str;
+			
+	var title = $el.find('.poem-title').html();
+	var verses = $el.find('.poem-body').html();
 
-var stanza = {
-		1: "That time of year",
-		2: "Thou mayst in me behold",
-		3: "When yellow leaves or none or few do hang",
-		4: "From broken bows that shake against the cold",
-		5: "Where late the sweet birds sang"
-	};
-
-function createInput ( field ) { // my great change
-	var input = document.createElement("input");
-	
-	input.type = "text";
-	input.className = "button";
-	input.value = field;
-	return input;
-}
-
-function printButtons( buttons ) {
-	var div = document.getElementById("poemsam"),
-	buttonsLen = buttons.length;
-	
-	for ( var i = 0; i < buttonsLen; i++ ) {
-		div.appendChild(buttons[i]);
+	function makeBrElements(wordsAndSpaces) {
+		wordsAndSpaces.forEach( function(val,idx,arr) {
+			arr[idx] = val.replace(/\r\n/g,'<br/>').replace(/[\r\n]/g,'<br/>');
+		});
+		return wordsAndSpaces;
 	}
-}
 
-function getInput( verse ) {
-	var line = verse.split(" "),
-	lineLen = line.length,
-	htm = [];
-
-	for ( var i = 0; i < lineLen; i++ ) {
-		htm.push( createInput( line[i] ) );
+	function makeNbspElements(wordsAndBrEntities) {
+		wordsAndBrEntities.forEach( function(val,idx,arr) {
+			arr[idx] = val.replace(/\s/g,'&nbsp;');
+		});
+		return wordsAndBrEntities;
 	}
-	// add a line break to every verse
-	htm.push( document.createElement("br") );
-	htm.push( document.createElement("br") );
-	printButtons(htm);
-}
 
-function getEachLine(stanza) {
-	for ( var verse in stanza ) {
-		getInput( stanza[verse] );
+	function makePoemEntity(wordsAndBrNbspEntities) {
+		var poemEntity = "";
+		wordsAndBrNbspEntities.forEach( function(val) {
+			poemEntity += val;
+		});
+		return poemEntity;
 	}
-}
 
-getEachLine(stanza);
+	wordsAndSpaces = verses.split(/([^\s]+)/);
+	wordIndexes = wordsAndSpaces.map(function(el,idx){
+		return /\s+/.test(el) ? undefined : idx;
+	})
+	.filter(function(el){
+		return el !== undefined;
+	});
+
+	wordsAndBrEntities = makeBrElements(wordsAndSpaces);
+	wordsAndBrNbspEntities = makeNbspElements(wordsAndBrEntities);
+	buildPoem = makePoemEntity(wordsAndBrNbspEntities);
+	$print.append(buildPoem);
+}());
+
