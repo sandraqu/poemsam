b6ccfb108e877989f3cc01a99c83ab189a154c7f
Sandra Quiles <squiles@bamboorose.com>
2016-09-23 09:26:48 -0400

    make shuffle for loop to forEach

--- poemsam.js
+++ poemsam.js
@@ -57,12 +57,12 @@
 	 * Using Durstenfeld shuffle algorithm.
 	 */
 	function shuffleArray(arr) {
-    for (var i = arr.length - 1; i > 0; i--) {
+		arr.forEach( function(val,i) {
       var j = Math.floor(Math.random() * (i + 1));
       var temp = arr[i];
       arr[i] = arr[j];
       arr[j] = temp;
-    };
+    });
     return arr;
 	}
 
