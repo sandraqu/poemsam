c0a2da79a53bcc9e5e9f68c234a8616e50dd5cae
Sandra Quiles <squiles@bamboorose.com>
2016-10-19 10:50:44 -0400

    long overdue change.  letting css handle space with pre.  handling selection through DOM element.  the goal is to have only ONE selection and avoid the mass removal of a class everytime a selection is made.  it is an idea.  we shall see how it translates.

--- poemsam.js
+++ poemsam.js
@@ -1,14 +1,17 @@
 (function() {
 	var	$new = $("#newPoem"),
 			$game = $('#poemGame'),
-			$poem = $('#poemsam'),
-			$buttons = $('#poemButtons'),
+			$poem = $game.find('.poem-sam'),
+			$buttons = $game.find('.poem-buttons'),
 			str;
 			
 	var title = $new.find('.poem-title').html();
 	var verses = $new.find('.poem-body').html();
 	var poemHidden = '.poem-hidden';
 	var poemWord = '.poem-word';
+	var sheetHidden = document.getElementById('styleHidden');
+	var sheetWord = document.getElementById('styleWord');
+
 
 	/*
 	function makeBrElements(wordsAndSpaces) {
@@ -26,7 +29,7 @@
 	}
 	*/
 
-	function selectOneInX(interval,wordIndexes) {
+	function selectOneInSet(setsOfIndexes,interval) {
 		var subsetOfIndexes=[],
 			finalIndexes=[],
 			rand;
@@ -42,9 +45,44 @@
 		return finalIndexes; // for hiding
 	}
 
+	function makeSubsets(wordIndexes,interval) {
+		var subsetOfIndexes=[],
+			sets=[];
+		// to dos
+		// first array ends up being interval+1
+		// also, any remainders after the last mod 0, don't get into the sets
+		wordIndexes.forEach( function(val,idx) {
+			subsetOfIndexes.push(val);
+			if(idx !== 0 && idx % interval === 0) {
+				sets.push(subsetOfIndexes);
+				subsetOfIndexes = [];
+			}
+		});
+		return sets;
+	}
+
+	function temp(listOfLists) {
+		var rand,
+			finalIndexes=[];
+
+		listOfLists.forEach( function(){
+			rand = subsetOfIndexes[Math.floor(Math.random() * subsetOfIndexes.length)];
+			finalIndexes.push(rand);
+		});
+		return finalIndexes;
+	}
+
+	function selectOneRandomFromSubset(mainArr) {
+    filter(mainArr, selectSubset).forEach(selectRandom);
+	}
+
+	function selectRandom(subsetOfIndexes) {
+		return subsetOfIndexes[Math.floor(Math.random() * subsetOfIndexes.length)];
+	}
+
 	function hideTheseWords(selectedIndexesForHiding,wordsAndBrNbspEntities) {
 		selectedIndexesForHiding.forEach( function(val,idx) {
-			wordsAndBrNbspEntities[val] = '<span id="holder-'+ idx +'" data-match="'+ val +'" class="mdl-chip poem-hidden" style="width:30px;"><span class="mdl-chip__text"></span></span>'
+			wordsAndBrNbspEntities[val] = '<label id="holder-'+ idx +'" data-match="'+ val +'" class="mdl-chip poem-hidden"><span class="mdl-chip__text"></span></label>'
 		});
 		return wordsAndBrNbspEntities;
 	}
@@ -58,23 +96,23 @@
 	}
 
 	/**
-	 * Randomize array element order in-place.
-	 * Using Durstenfeld shuffle algorithm.
-	 */
+	* Randomize array element order in-place.
+	* Using Durstenfeld shuffle algorithm.
+	*/
 	function shuffleArray(arr) {
 		arr.forEach( function(val,i) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
-    });
-    return arr;
+			var j = Math.floor(Math.random() * (i + 1));
+			var temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		});
+		return arr;
 	}
 
 	function makeWordButtons(suffledIndexesForHiding,wordsAndBrNbspEntities) {
 		var buttonEntities = [];
 		suffledIndexesForHiding.forEach( function(val,idx) {
-			buttonEntities.push('<span id="guess-'+ idx +'" data-match="'+ val +'" class="mdl-chip poem-word"><span class="mdl-chip__text">'+wordsAndBrNbspEntities[val]+'</span></span>');
+			buttonEntities.push('<label id="guess-'+ idx +'" data-match="'+ val +'" class="mdl-chip poem-word"><span class="mdl-chip__text">'+wordsAndBrNbspEntities[val]+'</span></label>');
 		});
 		return buttonEntities;
 	}
@@ -92,10 +130,21 @@
 			return hey.replace(/\r\n/g,'<br/>').replace(/[\r\n]/g,'<br/>');
 		});
 	//wordsAndBrNbspEntities = makeNbspElements(wordsAndBrEntities);
+	/*
 	wordsAndBrNbspEntities = wordsAndBrEntities.map( function(now) {
 			return now.replace(/\s/g,'&nbsp;');
 		});
-	selectedIndexesForHiding = selectOneInX(5,wordIndexes);
+		*/
+	//selectedIndexesForHiding = selectOneInX(wordIndexes,5);
+	setsOfIndexes = makeSubsets(wordIndexes,5);
+
+	// Cache your array!
+	// store last poem (new poem) in local storage
+
+	selectedIndexesForHiding = setsOfIndexes.map( function(hey){
+			return hey[Math.floor(Math.random() * hey.length)];
+		});
+
 	suffledIndexesForHiding = shuffleArray(selectedIndexesForHiding);
 	//// encrypt match number
 	// on button click, find active holder and enter
@@ -103,8 +152,8 @@
 	//// decrypt match number
 	
 	// does guess match index
-	buttonDomElements = makeWordButtons(suffledIndexesForHiding,wordsAndBrNbspEntities);
-	wordsAndHiddenEntities = hideTheseWords(selectedIndexesForHiding,wordsAndBrNbspEntities);
+	buttonDomElements = makeWordButtons(suffledIndexesForHiding,wordsAndBrEntities);
+	wordsAndHiddenEntities = hideTheseWords(selectedIndexesForHiding,wordsAndBrEntities);
 
 	buildButtons = makeDomEntity(buttonDomElements);
 	buildPoem = makeDomEntity(wordsAndHiddenEntities);
@@ -115,23 +164,83 @@
 		active = $('.active').length;
 		if( active === 2) {
 			wordMatch = $buttons.find('.active').data('match');
+			wordWidth = $buttons.find('.active').width();
 			blankMatch = $poem.find('.active').data('match');
 			if ( wordMatch === blankMatch) {
 				word = $buttons.find('.active').text();
-				$poem.find('.active').attr('style','').find('.mdl-chip__text').text(word);
+				$poem.find('.active').attr('style','').width(wordWidth);
+				$poem.find('.active').find('.mdl-chip__text').addClass('done').text(word);
 			}
 		}
 	}
 
-	$("body").on("click", poemWord, function() {
-		$(poemWord).removeClass('active');
-		$(this).addClass('active');
+	function isSelf(hey, obj) {
+		var regx = new RegExp(hey,"g");
+		var str = obj.innerText; // HTMLStyleElement, innerText is inherited from HTMLElement
+
+		return regx.test(str);
+	}
+
+	function makeStyle(guess,word){
+		// modify css instead
+		var style = document.createElement('style');
+		style.type = 'text/css';
+		style.innerHTML = '.cssClass { color: #F00; }';
+		document.getElementsByTagName('head')[0].remove(style);
+		document.getElementsByTagName('head')[0].appendChild(style);
+
+		//document.getElementById('someElementId').className = 'cssClass';
+	}
+	// insert Alman's code learning here
+
+	$("body").on("click", poemWord, function(event) {
+		event.preventDefault();
+		// <label id="guess-2" data-match="101" class="mdl-chip poem-word"><span class="mdl-chip__text">because</span></label>
+		// this.id;
+		// <div class="poem-word" data-id="" data-play=""></div>
+		//debugger;
+		var $button = $(this);
+		//var id;
+		var data = $button.data("stats");
+		var style, hasStyleAlready;
+		
+		if (!data){
+			data = {};
+			data.id = $button.attr("id");
+			data.width = $button.width();
+			data.try = $button.data('match');
+			$button.data("stats", data); // update data
+		}
+ 
+    hasStyleAlready = isSelf(data.id,sheetWord);
+    style  = !hasStyleAlready ? "#"+data.id+"{ border-color: #ff5722; background-color: #f9d62e; }" : "";
+
+    sheetWord.innerHTML = style;
+		
+
+/*
+		if( $(this).hasClass('active') ){
+			$(this).toggleClass('active');	
+		} else {
+			$(poemWord).removeClass('active');
+			$(this).addClass('active');
+		}
 		checkMate();
+*/
 	});
 
+
+
+
 	$("body").on("click", poemHidden, function() {
-		$(poemHidden).removeClass('active');
-		$(this).addClass('active');
+		    // <div class="poem-blank" data-id="" data-play=""></div>
+
+		if( $(this).hasClass('active') ){
+			$(this).toggleClass('active');	
+		} else {
+			$(poemHidden).removeClass('active');
+			$(this).addClass('active');
+		}
 		checkMate();
 	});
 
